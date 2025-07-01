18ec665833ae65ec89cf29619279a7a0c89e8a56
Frank Hunleth <fhunleth@troodon-software.com>
2025-07-01 09:07:01 -0400

    hailort: use hailort-supplied libraries for now

--- packages/hailort/hailort.mk
+++ packages/hailort/hailort.mk
@@ -9,7 +9,15 @@ HAILORT_SITE = $(call github,hailo-ai,hailort,v$(HAILORT_VERSION))
 HAILORT_LICENSE = MIT
 HAILORT_LICENSE_FILES = hailort/LICENSE hailort/LICENSE-3RD-PARTY.md
 HAILORT_SUPPORTS_IN_SOURCE_BUILD = NO
+HAILORT_INSTALL_STAGING = YES
 
-HAILORT_DEPENDENCIES = spdlog_hailort protobuf eigen
+HAILORT_DEPENDENCIES = spdlog_hailort
+
+define HAILORT_INSTALL_PROTOBUF_LITE
+         $(INSTALL) -D -m 0644 $(@D)/buildroot-build/_deps/protobuf-build/libprotobuf-lite.so.32 \
+                $(TARGET_DIR)/usr/lib/libprotobuf-lite.so.32
+endef
+
+HAILORT_POST_INSTALL_TARGET_HOOKS += HAILORT_INSTALL_PROTOBUF_LITE
 
 $(eval $(cmake-package))
