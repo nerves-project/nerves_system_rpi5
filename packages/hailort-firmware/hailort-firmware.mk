a3d65f42d853ec0460b79cbdf8019f9b3a660c82
Frank Hunleth <fhunleth@troodon-software.com>
2025-07-01 20:24:50 -0400

    hailort-firmware: new package
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>

---
+++ packages/hailort-firmware/hailort-firmware.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# hailort-firmware
+#
+################################################################################
+
+HAILORT_FIRMWARE_VERSION = 4.20.0
+HAILORT_FIRMWARE_SITE = https://hailo-hailort.s3.eu-west-2.amazonaws.com/Hailo8/$(HAILORT_FIRMWARE_VERSION)/FW
+HAILORT_FIRMWARE_SOURCE = hailo8_fw.$(HAILORT_FIRMWARE_VERSION).bin
+HAILORT_FIRMWARE_LICENSE = Proprietary
+
+define HAILORT_FIRMWARE_EXTRACT_CMDS
+	cp $(HAILORT_FIRMWARE_DL_DIR)/$(HAILORT_FIRMWARE_SOURCE) $(@D)
+endef
+
+define HAILORT_FIRMWARE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/$(HAILORT_FIRMWARE_SOURCE) \
+		$(TARGET_DIR)/lib/firmware/hailo/hailo8_fw.bin
+endef
+
+$(eval $(generic-package))
